2f63aff9f0953944658db85bca95f2d2a0615d5b
Payson <pscush@gmail.com>
2023-06-30 14:49:59 -0700

    toggle cluster map. fixed typo in story map

--- cluster_map/static/js/logic.js
+++ cluster_map/static/js/logic.js
@@ -12,13 +12,16 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 let baseURL = "https://data.sfgov.org/resource/pyih-qa8i.json?";
 // let url = baseURL + "$$app_token=" + apiToken;
 
+// Create an array to store the markers
+let markers = [];
+
+// Create a marker cluster group
+let markerCluster = L.markerClusterGroup();
+
 // Get the data with fetch.
 fetch(baseURL)
   .then(response => response.json())
   .then(data => {
-    // Create a new marker cluster group.
-    let markers = L.markerClusterGroup();
-
     // Loop through the data
     data.forEach(restaurant => {
       // Get the latitude and longitude
@@ -45,18 +48,20 @@ fetch(baseURL)
                        <strong>INSPECTION SCORE:</strong><br> 
                        <span style="font-size: 26px; background-color: ${scoreColor}; color: black; padding: 2px 5px; border-radius: 4px;">${restaurant.inspection_score}</span>
                      </div>`);
-        // Add the marker to the cluster group
-        markers.addLayer(marker);
+
+        // Add the marker to the array and the cluster group
+        markers.push(marker);
+        markerCluster.addLayer(marker);
       }
     });
 
     // Add the marker cluster group to the map
-    myMap.addLayer(markers);
+    myMap.addLayer(markerCluster);
 
     // Create the search control
     var controlSearch = new L.Control.Search({
       position: 'topright',
-      layer: markers,
+      layer: markerCluster,
       initial: false,
       zoom: 12,
       marker: false
@@ -64,4 +69,68 @@ fetch(baseURL)
 
     // Add the search control to the map
     myMap.addControl(controlSearch);
+
+    // Create the score filter control
+    var controlScores = L.control({ position: 'topright' });
+
+    controlScores.onAdd = function (map) {
+      var div = L.DomUtil.create('div', 'info legend');
+      div.innerHTML += '<h4>Filter by Score</h4>';
+
+      // Define the score ranges
+      var scoreRanges = [
+        { color: 'green', min: 90, max: Infinity },
+        { color: 'yellow', min: 75, max: 89 },
+        { color: 'red', min: 0, max: 74 }
+      ];
+
+      // Create checkboxes for each score range
+      scoreRanges.forEach(range => {
+        div.innerHTML +=
+          '<label><input type="checkbox" class="score-checkbox" value="' +
+          range.color +
+          '" checked>' +
+          range.min +
+          ' - ' +
+          (range.max === Infinity ? '∞' : range.max) +
+          '</label><br>';
+      });
+
+      // Add event listener to handle checkbox changes
+      div.addEventListener('change', function (event) {
+        var checkboxes = document.getElementsByClassName('score-checkbox');
+
+        // Create an array to store the checked score colors
+        var checkedColors = [];
+
+        // Loop through checkboxes and update marker visibility
+        Array.from(checkboxes).forEach(checkbox => {
+          var scoreColor = checkbox.value;
+          var isChecked = checkbox.checked;
+
+          if (isChecked) {
+            checkedColors.push(scoreColor); // Store the checked score colors
+          }
+        });
+
+        // Remove all markers from the cluster group
+        markerCluster.clearLayers();
+
+        // Loop through the markers and add back the ones with matching score colors
+        markers.forEach(marker => {
+          var markerScoreColor = marker._popup._content
+            .split('background-color: ')[1]
+            .split(';')[0];
+
+          if (checkedColors.includes(markerScoreColor)) {
+            markerCluster.addLayer(marker); // Add the marker back to the cluster group
+          }
+        });
+      });
+
+      return div;
+    };
+
+    // Add the score filter control to the map
+    controlScores.addTo(myMap);
   });
