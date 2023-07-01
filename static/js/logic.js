7c1c8c9cb26d614867fdd55a887e829a526b1ffc
dalfonsonash <dalfonsonash@outlook.com>
2023-07-01 15:44:42 -0700

    Made correction to GeoJSON script using d3.

--- static/js/logic.js
+++ static/js/logic.js
@@ -8,32 +8,13 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
 }).addTo(myMap);
 
-// Store the API query variables.
+// Link to GeoJSON data.
 let geoData = "https://data.sfgov.org/resource/6ia5-2f8k.geojson"; 
 
-let geojson;
-// Fetch the GeoJSON data
-fetch(geoData)
-  .then(response => response.json())
-  .then(data => {
-    // Create a choropleth layer based on the GeoJSON data
-    geojson = L.choropleth(data, {
-      valueProperty: 'Nob Hill', // Replace 'property_name' with the actual property name in your GeoJSON data that represents the values for the choropleth
-      scale: ["#ffffb2", "#b10026"], // Define the colors for the choropleth scale
-      steps: 5, // Define the number of color steps or ranges
-      mode: 'q', // Define the mode for determining the color ranges ('q' for quantile, 'e' for equidistant, or 'k' for k-means clustering)
-      style: {
-        color: '#fff', // Border color for the choropleth polygons
-        weight: 1, // Border weight for the choropleth polygons
-        fillOpacity: 0.8 // Opacity for the choropleth polygons
-      },
-      onEachFeature: function(feature, layer) {
-        // Add any desired interaction or information for each choropleth polygon
-        layer.bindPopup('Neighborhood: ' + feature.properties.name); // Replace 'name' with the actual property name in your GeoJSON data that represents the neighborhood name
-      }
-    });
 
-    // Add the choropleth layer to the map
-    geojson.addTo(myMap);
-  });
+// Getting the GeoJSON data
+d3.json(geoData).then(function(data){
 
+  // Creating GeoJSON layer.
+  L.geoJson(data).addTo(myMap);
+});
