12143f941dab6a461d705c149b18f5ff30949d43
dalfonsonash <dalfonsonash@outlook.com>
2023-07-01 12:25:52 -0700

    Javacript for choroplet map adding to my branch.

--- static/js/logic.js
+++ static/js/logic.js
@@ -10,48 +10,75 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 // Store the API query variables.
 let baseURL = "https://data.sfgov.org/resource/pyih-qa8i.json?";
-// let url = baseURL + "$$app_token=" + apiToken;
-// Get the data with fetch.
-let markers = L.markerClusterGroup();
+let neighborhoodURL = "https://data.sfgov.org/api/geospatial/pty2-tcw4?method=export&format=GeoJSON"; // Replace with the actual URL or file path of the neighborhood GeoJSON data.
 
-// Get the data with fetch.
-fetch(baseURL)
+// Fetch the neighborhood GeoJSON data
+fetch(neighborhoodURL)
   .then(response => response.json())
-  .then(data => {
-    // Create a new marker cluster group.
-    let markers = L.markerClusterGroup();
+  .then(neighborhoodData => {
+    // Create a data structure to store average inspection scores by neighborhood
+    let averageScoresByNeighborhood = {};
 
-    // Loop through the data
-    data.forEach(restaurant => {
-      // Get the latitude and longitude
-      let latitude = parseFloat(restaurant.business_latitude);
-      let longitude = parseFloat(restaurant.business_longitude);
-      let inspectionScore = restaurant.inspection_score;
-    
-      let scoreColor;
-      if (inspectionScore >= 90) {
-        scoreColor = 'green';
-      } else if (inspectionScore >= 75 && inspectionScore <= 89) {
-        scoreColor = 'yellow';
-      } else {
-        scoreColor = 'red';
-      }
-    
-      // Check for valid latitude and longitude
-      if (!isNaN(latitude) && !isNaN(longitude)) {
-        // Create a marker and bind a popup
-        let marker = L.marker([latitude, longitude])
-          .bindPopup(`<div style="text-align: center;">
-                       <strong style="font-size: 14px;">${restaurant.business_name}</strong><br>
-                       <strong>ADDRESS:</strong><br> ${restaurant.business_address}<br>
-                       <strong>INSPECTION SCORE:</strong><br> 
-                       <span style="font-size: 26px; background-color: ${scoreColor}; color: black; padding: 2px 5px; border-radius: 4px;">${restaurant.inspection_score}</span>
-                     </div>`);
-        // Add the marker to the cluster group
-        markers.addLayer(marker);
-      }
-    });
-    
-    // Add the marker cluster group to the map
-    myMap.addLayer(markers);
+    // Fetch the restaurant data
+    fetch(baseURL)
+      .then(response => response.json())
+      .then(restaurantData => {
+        // Calculate average inspection scores by neighborhood
+        restaurantData.forEach(restaurant => {
+          let neighborhood = restaurant.neighborhood;
+          let inspectionScore = restaurant.inspection_score;
+
+          if (!averageScoresByNeighborhood[neighborhood]) {
+            averageScoresByNeighborhood[neighborhood] = {
+              totalScore: inspectionScore,
+              count: 1
+            };
+          } else {
+            averageScoresByNeighborhood[neighborhood].totalScore += inspectionScore;
+            averageScoresByNeighborhood[neighborhood].count++;
+          }
+        });
+
+        // Calculate the average score for each neighborhood
+        for (let neighborhood in averageScoresByNeighborhood) {
+          averageScoresByNeighborhood[neighborhood].averageScore = averageScoresByNeighborhood[neighborhood].totalScore / averageScoresByNeighborhood[neighborhood].count;
+        }
+
+        // Create the choropleth map layer
+        let geojsonLayer = L.geoJSON(neighborhoodData, {
+          style: function (feature) {
+            let neighborhood = feature.properties.neighborhood;
+            let averageScore = averageScoresByNeighborhood[neighborhood].averageScore;
+            let fillColor;
+
+            if (averageScore >= 90) {
+              fillColor = 'green';
+            } else if (averageScore >= 75 && averageScore <= 89) {
+              fillColor = 'yellow;
+            } else {
+              fillColor = 'red';
+            }
+
+            return {
+              fillColor: fillColor,
+              color: 'black',
+              weight: 1,
+              fillOpacity: 0.6
+            };
+          },
+          onEachFeature: function (feature, layer) {
+            let neighborhood = feature.properties.neighborhood;
+            let averageScore = averageScoresByNeighborhood[neighborhood].averageScore;
+
+            layer.bindPopup(`<div style="text-align: center;">
+                               <strong style="font-size: 14px;">${neighborhood}</strong><br>
+                               <strong>AVERAGE INSPECTION SCORE:</strong><br> 
+                               <span style="font-size: 26px; background-color: ${fillColor}; color: black; padding: 2px 5px; border-radius: 4px;">${averageScore}</span>
+                             </div>`);
+          }
+        });
+
+        // Add the choropleth layer to the map
+        myMap.addLayer(geojsonLayer);
+      });
   });
