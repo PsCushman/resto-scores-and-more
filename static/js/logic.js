0ad21e1fe1d0b50dce8978fcd7e2befb00777a88
dalfonsonash <dalfonsonash@outlook.com>
2023-07-01 17:02:50 -0700

    More changes to choropleth js. Making some progress.

--- static/js/logic.js
+++ static/js/logic.js
@@ -8,67 +8,63 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
 }).addTo(myMap);
 
-// Link to GeoJSON data.
-let geoData = "https://data.sfgov.org/resource/6ia5-2f8k.geojson"; 
+// Link to GeoJSON data
+let geoData = "https://data.sfgov.org/resource/6ia5-2f8k.geojson";
+// Link to JSON data
+let jsonData = "https://data.sfgov.org/resource/pyih-qa8i.json";
 
-// Define style object.
-let mapStyle ={
-  color: "blue",
-  fillOpacity: 0.5,
-  weight: 1.5
-}
-
-// Fetch the JSON data.
-fetch("https://data.sfgov.org/resource/pyih-qa8i.json")
+// Fetching the JSON data
+fetch(jsonData)
   .then(response => response.json())
   .then(jsonData => {
-    // Group inspection scores by coordinate and calculate average
-    let averageScoresByCoordinate = {};
-    jsonData.forEach(item => {
-      let coordinate = item.business_location && item.business_location.coordinates;
-      if (coordinate && item.inspection_score) {
-        if (!averageScoresByCoordinate[coordinate]) {
-          averageScoresByCoordinate[coordinate] = {
-            totalScore: 0,
-            count: 0
-          };
-        }
-        averageScoresByCoordinate[coordinate].totalScore += Number(item.inspection_score);
-        averageScoresByCoordinate[coordinate].count++;
-      }
-    });
-
-    // Calculate average scores
-    for (let coordinate in averageScoresByCoordinate) {
-      let averageScore = averageScoresByCoordinate[coordinate].totalScore / averageScoresByCoordinate[coordinate].count;
-      averageScoresByCoordinate[coordinate] = averageScore;
-    }
-
     // Getting the GeoJSON data
-    d3.json(geoData).then(function(data){
-      // Creating GeoJSON layer.
+    d3.json(geoData).then(function(data) {
+      // Creating GeoJSON layer with choropleth
       L.geoJson(data, {
-        style: function (feature) {
-          let coordinate = feature.geometry.coordinates;
-          let averageScore = averageScoresByCoordinate[coordinate];
+        style: function(feature) {
+          // Retrieve the relevant property for the choropleth
+          let score = feature.properties.inspection_score;
+
+          // Define the color based on the score value
+          let fillColor = "#808080"; // Default color for missing values
+          if (score >= 90) {
+            fillColor = "#008000"; // Green
+          } else if (score >= 80) {
+            fillColor = "#FFFF00"; // Yellow
+          } else if (score >= 0) {
+            fillColor = "#FF0000"; // Red
+          }
+
+          // Return the style object
           return {
-            fillColor: getFillColor(averageScore),
             color: "blue",
+            fillColor: fillColor,
             fillOpacity: 0.5,
             weight: 1.5
           };
         },
-        onEachFeature: function (feature, layer){
-          let coordinate = feature.geometry.coordinates;
-          let averageScore = averageScoresByCoordinate[coordinate];
+        onEachFeature: function(feature, layer) {
+          // Find the corresponding business in the JSON data
+          let filteredData = jsonData.filter(
+            item => item.business_id === feature.properties.business_id
+          );
 
           // Create the popup content
-          let popupContent = "<strong>" + feature.properties.business_name + "</strong><br>" +
-            "Address: " + feature.properties.business_address + "<br>" +
-            "City: " + feature.properties.business_city + "<br>" +
-            "State: " + feature.properties.business_state + "<br>" +
-            "Postal Code: " + feature.properties.business_postal_code + "<br>" +
-            "Average Inspection Score: " + averageScore;
+          let popupContent = "<strong>" + feature.properties.name + "</strong><br>";
+
+          if (filteredData.length > 0) {
+            let jsonDataItem = filteredData[0];
+
+            // Add additional data from the JSON
+            popupContent +=
+              "Address: " + (jsonDataItem.business_address || feature.properties.address) + "<br>";
+
+            if (jsonDataItem.business_location && jsonDataItem.business_location.coordinates) {
+              let coordinates = jsonDataItem.business_location.coordinates;
+              popupContent += "Latitude: " + coordinates[1] + "<br>";
+              popupContent += "Longitude: " + coordinates[0] + "<br>";
+            }
+          }
 
           // Bind the popup to the layer
           layer.bindPopup(popupContent);
@@ -77,14 +73,3 @@ fetch("https://data.sfgov.org/resource/pyih-qa8i.json")
     });
   })
   .catch(error => console.log(error));
-
-// Define a function to determine fill color based on inspection score
-function getFillColor(score) {
-  if (score >= 90) {
-    return "#008000"; // Green
-  } else if (score >= 80) {
-    return "#FFFF00"; // Yellow
-  } else {
-    return "#FF0000"; // Red
-  }
-}
