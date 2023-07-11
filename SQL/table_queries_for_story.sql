e2421bf306863ab2864b78f563528a20c65fd319
Payson <pscush@gmail.com>
2023-07-11 02:58:10 -0400

    fixed sql imports and queries. will update map

--- SQL/table_queries_for_story.sql
+++ SQL/table_queries_for_story.sql
@@ -1,96 +1,113 @@
 SELECT * FROM for_maps_table;
 
--- Most High Risk Incidents
-SELECT business_name, COUNT(*) AS high_risk_incidents
+-- Most High Risk Incidents:
+SELECT business_id, business_name, COUNT(*) AS high_risk_incidents
 FROM for_maps_table
 WHERE inspection_date BETWEEN '2016-10-04' AND '2019-10-03'
   AND risk_category = 'High Risk'
-GROUP BY business_name
+GROUP BY business_id, business_name
 ORDER BY high_risk_incidents DESC
 LIMIT 5;
+-- "KING OF THAI NOODLE HOUSE" really a bunch with 3
 
--- Lee's Deli 17 Incidents 37.792828 -122.403575
-
--- Most Incidents overall
-SELECT business_name, COUNT(DISTINCT violation_id) AS total_incidents
+-- Most Incidents Overall:
+SELECT business_id, business_name, COUNT(DISTINCT violation_id) AS total_incidents
 FROM for_maps_table
 WHERE inspection_date BETWEEN '2016-10-04' AND '2019-10-03'
-GROUP BY business_name
+GROUP BY business_id, business_name
 ORDER BY total_incidents DESC
 LIMIT 5;
 
--- "Peet's Coffee & Tea"	74 37.781127 -122.400109
--- "Lee's Deli"	56
+-- Gateway High/Kip Schools 7
+-- "TAQUERIA CANCUN" 6 -ate here a lot
 
--- Most High Risk Incidents
-SELECT business_name, COUNT(*) AS high_risk_incidents
+--Who Had the most Inspections?:
+SELECT business_id, business_name, COUNT(*) AS inspection_count
 FROM for_maps_table
 WHERE inspection_date BETWEEN '2016-10-04' AND '2019-10-03'
-  AND risk_category = 'High Risk'
-GROUP BY business_name
-ORDER BY high_risk_incidents DESC
+GROUP BY business_id, business_name
+ORDER BY inspection_count DESC
 LIMIT 5;
 
--- Lowest AVG Score 
-SELECT business_name, AVG(inspection_score) AS average_score
+-- Schools and Taqeria Cancun
+
+-- Lowest AVG Score:
+SELECT business_id, business_name, AVG(inspection_score) AS average_score
 FROM for_maps_table
 WHERE inspection_date BETWEEN '2016-10-04' AND '2019-10-03'
-GROUP BY business_name
+GROUP BY business_id, business_name
 ORDER BY average_score ASC
 LIMIT 5;
 
--- "SUNFLOWER RESTAURANT" 56.2941176470588235
+-- "New Jumbo Seafood Restaurant"	60.5000000000000000	2
+-- "SUNFLOWER RESTAURANT"	63.5000000000000000	2
 
--- How Many Good Spots Are there?
+-- Really what we want is, lowest score and most incidents per visit:
+SELECT 
+  business_id, 
+  business_name, 
+  COUNT(*) AS total_incidents, 
+  COUNT(*) / COUNT(DISTINCT inspection_date) AS incidents_per_visit, 
+  AVG(inspection_score) AS average_score,
+  COUNT(DISTINCT inspection_date) AS total_visits
+FROM for_maps_table
+WHERE inspection_date BETWEEN '2016-10-04' AND '2019-10-03'
+GROUP BY business_id, business_name
+ORDER BY incidents_per_visit DESC, average_score ASC
+LIMIT 5;
+
+-- "New Jumbo Seafood Restaurant"	2	1	60.5000000000000000
+-- "SUNFLOWER RESTAURANT"	2	1	63.5000000000000000
+
+-- How Many Good Spots Are There?:
 SELECT COUNT(*) AS place_count
 FROM (
-  SELECT business_name, AVG(inspection_score) AS average_score
+  SELECT business_id, business_name, AVG(inspection_score) AS average_score
   FROM for_maps_table
   WHERE inspection_date BETWEEN '2016-10-04' AND '2019-10-03'
-  GROUP BY business_name
+  GROUP BY business_id, business_name
   HAVING COUNT(*) > 4
 ) AS subquery
-WHERE average_score > 95;
--- 159
+WHERE average_score > 90;
 
+-- How Many where Schools?:
 SELECT COUNT(*) AS school_count
 FROM (
-  SELECT business_name
+  SELECT business_id, business_name
   FROM for_maps_table
   WHERE inspection_date BETWEEN '2016-10-04' AND '2019-10-03'
-  GROUP BY business_name
-  HAVING COUNT(*) > 4 AND AVG(inspection_score) > 95 AND LOWER(business_name) LIKE '%school%'
+  GROUP BY business_id, business_name
+  HAVING COUNT(*) > 4 AND AVG(inspection_score) > 90 AND LOWER(business_name) LIKE '%school%'
 ) AS subquery;
--- Of the 156, 76 were Schools 
 
--- Which School did the best?
-SELECT business_name, AVG(inspection_score) AS average_score
+-- 11 of the 23 where schools
+
+-- Which School Did the Best?:
+SELECT business_id, business_name, AVG(inspection_score) AS average_score
 FROM for_maps_table
 WHERE inspection_date BETWEEN '2016-10-04' AND '2019-10-03'
-GROUP BY business_name
-HAVING COUNT(*) > 4 AND AVG(inspection_score) > 95 AND LOWER(business_name) LIKE '%school%';
+GROUP BY business_id, business_name
+HAVING COUNT(*) > 4 AND AVG(inspection_score) > 95 AND LOWER(business_name) LIKE '%school%'
+ORDER BY average_score DESC;
 
--- "ALICE FONG YU ELEMENTARY SCHOOL"
+-- "Francisco Middle School"	96.4000000000000000
+-- "Everett Middle School"	96.1666666666666667
 
--- Which School had the lowest Score
-SELECT business_name, AVG(inspection_score) AS average_score
+-- Which School Had the Lowest Score?:
+SELECT business_id, business_name, AVG(inspection_score) AS average_score
 FROM for_maps_table
 WHERE inspection_date BETWEEN '2016-10-04' AND '2019-10-03'
-GROUP BY business_name
+GROUP BY business_id, business_name
 HAVING COUNT(*) > 4 AND LOWER(business_name) LIKE '%school%'
 ORDER BY average_score ASC
 LIMIT 1;
 
--- "Sts. Peter & Paul School"	80.1428571428571429
-
--- Which School had the most number High Risk Incidents
-SELECT business_name, COUNT(*) AS high_risk_incidents
+-- Which School Had the Most Number of High Risk Incidents?:
+SELECT business_id, business_name, COUNT(*) AS high_risk_incidents
 FROM for_maps_table
 WHERE inspection_date BETWEEN '2016-10-04' AND '2019-10-03'
   AND risk_category = 'High Risk'
   AND LOWER(business_name) LIKE '%school%'
-GROUP BY business_name
+GROUP BY business_id, business_name
 ORDER BY high_risk_incidents DESC
 LIMIT 1;
-
--- "Sts. Peter & Paul School"	3
--- SQL/table_schema.sql
+++ SQL/table_schema.sql
@@ -4,7 +4,7 @@ DROP TABLE IF EXISTS inspection_info_table;
 DROP TABLE IF EXISTS for_maps_table;
 
 CREATE TABLE IF NOT EXISTS useful_table (
-    business_id INT,
+    business_id INT PRIMARY KEY,
     business_name VARCHAR,
     business_address VARCHAR,
     business_city VARCHAR,
@@ -17,20 +17,22 @@ CREATE TABLE IF NOT EXISTS useful_table (
     violation_id VARCHAR,
     violation_description VARCHAR,
     risk_category VARCHAR
+	FOREIGN KEY (business_id) REFERENCES business_info_table (business_id)
 );
 
 CREATE TABLE IF NOT EXISTS inspection_info_table (
     business_id INT,
-    inspection_id VARCHAR,
+    inspection_id VARCHAR PRIMARY KEY,
     inspection_date DATE,
     inspection_score INT,
     violation_id VARCHAR,
     violation_description VARCHAR,
     risk_category VARCHAR
+	FOREIGN KEY (business_id) REFERENCES business_info_table (business_id)
 );
 
 CREATE TABLE IF NOT EXISTS business_info_table (
-    business_id INTEGER,
+    business_id INT PRIMARY KEY,
     business_name VARCHAR,
     business_address VARCHAR,
     business_city VARCHAR,
@@ -40,16 +42,18 @@ CREATE TABLE IF NOT EXISTS business_info_table (
 );
 
 CREATE TABLE IF NOT EXISTS for_maps_table (
+	business_id INT,
     business_name VARCHAR,
     business_address VARCHAR,
     business_latitude NUMERIC,
     business_longitude NUMERIC,
     business_location VARCHAR,
     inspection_date DATE,
-    inspection_score NUMERIC,
+    inspection_score Numeric (10,2),
     violation_id VARCHAR,
     violation_description VARCHAR,
-    risk_category VARCHAR
+    risk_category VARCHAR,
+	PRIMARY KEY (business_id, inspection_date)
 );
 
 SELECT * FROM useful_table;
