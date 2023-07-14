9c684700f726c2b1382dad3449320842cbf74913
dalfonsonash <dalfonsonash@outlook.com>
2023-07-13 19:20:34 -0700

    Map updated by neighborhood.

--- static/js/logic.js
+++ static/js/logic.js
@@ -10,149 +10,143 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(myMap);
 
 // D3 to get the JSON data
-d3.json("https://data.sfgov.org/resource/pyih-qa8i.json").then(function(data) {
-  // Filter out businesses without zip codes or inspection scores
-  let filteredData = data.filter(item => item.business_postal_code && item.inspection_score !== undefined);
-
-  // Create an object to hold the inspection scores for each zip code
-  let inspectionScores = {};
-
-  // Iterate over the filtered data and store inspection scores by zip code
-  filteredData.forEach(item => {
-    let zipCode = item.business_postal_code;
-    let inspectionScore = parseFloat(item.inspection_score);
-
-    // Check if the zip code exists in the inspectionScores object
-    if (zipCode in inspectionScores) {
-      inspectionScores[zipCode].push(inspectionScore);
-    } else {
-      inspectionScores[zipCode] = [inspectionScore];
-    }
-  });
-
-  // Output inspection score averages and all inspection scores
-  Object.entries(inspectionScores).forEach(([zipCode, scores]) => {
-    let average = Math.round(scores.reduce((total, score) => total + score, 0) / scores.length);
-    inspectionScores[zipCode] = average; // Update inspectionScores object with rounded average score
-    console.log(`Zip Code: ${zipCode}, Average Inspection Score: ${average}`);
-    console.log(`Inspection Scores for ${zipCode}: ${scores}`);
-  });
-  
+d3.json("https://data.sfgov.org/resource/6ia5-2f8k.json").then(function(neighborhoodData) {
+  // Convert JSON data to GeoJSON format
+  let geojson = {
+    type: "FeatureCollection",
+    features: neighborhoodData.map(item => {
+      return {
+        type: "Feature",
+        properties: {
+          name: item.name,
+          coordinates: item.the_geom.coordinates
+        },
+        geometry: {
+          type: "MultiPolygon",
+          coordinates: item.the_geom.coordinates
+        }
+      };
+    })
+  };
 
   // D3 to get the JSON data
-  d3.json("https://data.sfgov.org/resource/srq6-hmpi.json").then(function(jsonData) {
-    // Convert JSON data to GeoJSON format
-    let geojson = {
-      type: "FeatureCollection",
-      features: jsonData.map(item => {
-        return {
-          type: "Feature",
-          properties: item,
-          geometry: {
-            type: "Polygon",
-            coordinates: item.geometry.coordinates
+  d3.json("https://data.sfgov.org/resource/pyih-qa8i.json").then(function(inspectionData) {
+    // Create an object to hold the inspection scores for each neighborhood
+    let inspectionScores = {};
+
+    // Group inspection scores by neighborhood
+    inspectionData.forEach(item => {
+      if (item.business_location && item.inspection_score !== undefined) {
+        let businessCoordinates = item.business_location.coordinates;
+        let inspectionScore = parseFloat(item.inspection_score);
+
+        // Check if the business coordinates fall within any neighborhood's coordinates
+        let neighborhood = geojson.features.find(feature =>
+          turf.booleanPointInPolygon(turf.point(businessCoordinates), feature.geometry)
+        );
+
+        if (neighborhood) {
+          let neighborhoodCoordinates = JSON.stringify(neighborhood.geometry.coordinates);
+
+          if (neighborhoodCoordinates in inspectionScores) {
+            inspectionScores[neighborhoodCoordinates].push(inspectionScore);
+          } else {
+            inspectionScores[neighborhoodCoordinates] = [inspectionScore];
           }
-        };
-      })
-    };
+        }
+      }
+    });
 
-    // Iterate over the geojson features and calculate the average inspection score for each zip code
+    // Iterate over the geojson features and calculate the average inspection score for each neighborhood
     geojson.features.forEach(feature => {
-      let zipCode = feature.properties.zip_code || feature.properties.zip || feature.properties.business_postal_code;
+      let coordinates = JSON.stringify(feature.geometry.coordinates);
 
-      // Check if the zip code exists in the inspectionScores object
-      if (zipCode in inspectionScores) {
-        let score = inspectionScores[zipCode];
-        feature.properties.average_inspection_score = score;
+      // Check if the coordinates exist in the inspectionScores object
+      if (coordinates in inspectionScores) {
+        let scores = inspectionScores[coordinates];
+        let average = Math.round(scores.reduce((total, score) => total + score, 0) / scores.length);
+        feature.properties.average_inspection_score = average;
       } else {
         feature.properties.average_inspection_score = undefined;
       }
     });
 
-    // Determine the minimum and maximum inspection scores
-    let scores = Object.values(inspectionScores).filter(score => score !== undefined);
-    let minScore = Math.min(...scores);
-    let maxScore = Math.max(...scores);
-
     // Create the choropleth layer using L.choropleth
-let choroplethLayer = L.choropleth(geojson, {
-  valueProperty: "average_inspection_score",
-  scale: ["#808080", "red", "yellow", "green"], // Gray, Red, Yellow, Green for the color scale
-  steps: 4,
-  mode: "q",
-  style: {
-    color: "blue",
-    weight: 0.5,
-    fillOpacity: 0.7
-  },
-  onEachFeature: (feature, layer) => {
-    let zipCode = feature.properties.zip_code || feature.properties.zip || feature.properties.business_postal_code;
-    let score = feature.properties.average_inspection_score;
-    let popupContent = "SF Public Health Data" + "<br>" + "Zip Code: " + zipCode + "<br>";
+    let choroplethLayer = L.choropleth(geojson, {
+      valueProperty: "average_inspection_score",
+      scale: ["#808080", "red", "yellow", "green"], // Gray, Red, Yellow, Green for the color scale
+      steps: 4,
+      mode: "q",
+      style: {
+        color: "gray",
+        weight: 0.5,
+        fillOpacity: 0.7
+      },
+      onEachFeature: (feature, layer) => {
+        let neighborhood = feature.properties.name;
+        let score = feature.properties.average_inspection_score;
+        let popupContent = "Neighborhood: " + neighborhood + "<br>";
 
-    if (score !== undefined) {
-      popupContent += "Average Inspection Score: " + (score ? score.toFixed(0) : "No data");
-    } else {
-      popupContent += "No data";
-    }
+        if (score !== undefined) {
+          popupContent += "Average Inspection Score: " + (score ? score.toFixed(0) : "No data");
+        } else {
+          popupContent += "No data";
+        }
 
-    layer.bindPopup(popupContent);
+        layer.bindPopup(popupContent);
 
-    layer.on("click", () => {
-      console.log(feature); // Log the feature data when clicked on the map
-    });
-  },
-  
-  // Assigning color range based on avg. insp. score.
-  getColor: (value) => {
-    if (value === undefined) {
-      return "#808080"; // Assign gray color to "No Data"
-    } else if (value >= 90) {
-      return "green"; // Assign green color to range 90 and above
-    } else if (value >= 85) {
-      return "yellow"; // Assign yellow color to range 85-89
-    } else if (value >= 81) {
-      return "red"; // Assign red color to range 81-84
-    } else {
-      return "#808080"; // Assign gray color to any other value outside the specified ranges
-    }
-  }
-  
-  
-}).addTo(myMap);
+        layer.on("click", () => {
+          console.log("Feature:", feature); // Log the feature data when clicked on the map
+          console.log("Average Inspection Score:", score); // Log the average inspection score
+        });
+      },
+      // Assigning color range based on avg. inspection score
+      getColor: (value) => {
+        if (value === undefined) {
+          return "#808080"; // Assign gray color to "No Data"
+        } else if (value >= 90) {
+          return "green"; // Assign green color to range 90 and above
+        } else if (value >= 85) {
+          return "yellow"; // Assign yellow color to range 85-89
+        } else if (value >= 81) {
+          return "red"; // Assign red color to range 81-84
+        } else {
+          return "#808080"; // Assign gray color to any other value outside the specified ranges
+        }
+      }
+    }).addTo(myMap);
 
-// Set up the legend.
-var legend = L.control({ position: "bottomright" });
-legend.onAdd = function() {
-  var div = L.DomUtil.create("div", "legend");
-  var limits = ["No Data", "81-84", "85-89", "90-100"];
-  var colors = ["#808080", "red", "yellow", "green"];
+    // Set up the legend.
+    var legend = L.control({ position: "bottomright" });
+    legend.onAdd = function() {
+      var div = L.DomUtil.create("div", "legend");
+      var limits = ["No Data", "81-84", "85-89", "90-100"];
+      var colors = ["#808080", "red", "yellow", "green"];
 
-  let labels = [];
+      let labels = [];
 
-  // Add the legend title.
-  div.innerHTML += '<h4 style="text-align: center;">SF Public Health Inspection Scores<br><span style="font-size: 14px; font-weight: bold;">by zip code</span></h4>';
+      // Add the legend title.
+      div.innerHTML += '<h4 style="text-align: center;">SF Public Health Inspection Scores<br><span style="font-size: 14px; font-weight: bold;">by neighborhood</span></h4>';
 
-  // Create the legend color bar.
-  for (var i = 0; i < limits.length; i++) {
-    var colorRange = limits[i];
-    div.innerHTML +=
-      '<i style="background:' + colors[i] + '"></i> ' +
-      colorRange + '<br>';
-  }
+      // Create the legend color bar.
+      for (var i = 0; i < limits.length; i++) {
+        var colorRange = limits[i];
+        div.innerHTML +=
+          '<i style="background:' + colors[i] + '"></i> ' +
+          colorRange + '<br>';
+      }
 
-  return div;
-};
+      return div;
+    };
 
-// Adding the legend to the map
-legend.addTo(myMap);
-
-
+    // Adding the legend to the map
+    legend.addTo(myMap);
 
     // Fit the map bounds to the choropleth layer
     myMap.fitBounds(choroplethLayer.getBounds());
-  })
-  .catch(function(error) {
-    console.error('Error fetching JSON data:', error);
+  }).catch(function(error) {
+    console.error("Error fetching inspection data:", error);
   });
+}).catch(function(error) {
+  console.error("Error fetching neighborhood data:", error);
 });
